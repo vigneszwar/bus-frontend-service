e7ce01f7e8b3180df1888a07214b62264bb85f7c
vigneszwar <vignezwar@gmail.com>
2023-07-23 10:43:08 +0530

    first commit

---
+++ src/test/java/com/bus/bus_backend_service/BusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bus.bus_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
